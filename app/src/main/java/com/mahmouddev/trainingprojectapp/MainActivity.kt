cfd2616c0df8eb009656857cf6f4eb828bc4a850
Mahmoud-Dev <mahmod-dev@outlook.com>
2021-06-30 18:52:00 +0300

    lecture #4

--- app/src/main/java/com/mahmouddev/trainingprojectapp/MainActivity.kt
+++ app/src/main/java/com/mahmouddev/trainingprojectapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.mahmouddev.trainingprojectapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import com.mahmouddev.trainingprojectapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e(TAG, "onCreate: " )
+
+        binding.btnLogin.setOnClickListener {
+
+            val intent = Intent(this,DetailsActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart: ", )
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume: ", )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause: ", )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "onStop: ", )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy: ", )
+    }
+
+
+
+
 }
